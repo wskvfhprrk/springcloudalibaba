bea356c7a6bee2be7687133cf23586d3115ab57a
hejz <75412985@qq.com>
2022-03-29 13:32:34 +0800

    添加nacos负载均衡轮询测试

--- stock/src/main/java/com/hejz/stock/Stock.java
+++ stock/src/main/java/com/hejz/stock/Stock.java
@@ -1,13 +1,18 @@
 package com.hejz.stock;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
 public class Stock {
+
+    @Value("${server.port}")
+    private String port;
+
     @GetMapping("destocking")
     public String destocking(){
         System.out.println("扣减库存成功!");
-        return "扣减库存成功";
+        return "扣减库存成功"+port;
     }
 }
