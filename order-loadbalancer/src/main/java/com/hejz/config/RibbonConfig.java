f551268f7fae75881ea83518e5631b5037f1dff4
hejz <75412985@qq.com>
2022-03-30 14:19:42 +0800

    添加loadbalancer

---
+++ order-loadbalancer/src/main/java/com/hejz/config/RibbonConfig.java
@@ -0,0 +1,24 @@
+/*package com.hejz.config;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+*//**
+ * 自定义ribbon轮询策略
+ *//*
+@Configuration
+public class RibbonConfig {
+
+    *//**
+     * 方法名一定是iRule
+     *
+     * @return
+     *//*
+    @Bean
+    public IRule iRule(){
+        //一种负载平衡策略，在现有服务器之间随机分配流量。
+        return new RandomRule();
+    }
+}*/
---
+++ order-loadbalancer/src/main/java/com/hejz/order/OrderApplication.java
@@ -0,0 +1,29 @@
+package com.hejz.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+    /**
+     * 调RestTemplate要配置RestTemplate，官方推荐一般使用builder建造方法
+     * @param builder
+     * @return
+     */
+    @Bean
+    @LoadBalanced //@LoadBalanced依赖负载均称衡器
+    public RestTemplate restTemplate(RestTemplateBuilder builder){
+        RestTemplate restTemplate = builder.build();
+        return restTemplate;
+    }
+
+}
