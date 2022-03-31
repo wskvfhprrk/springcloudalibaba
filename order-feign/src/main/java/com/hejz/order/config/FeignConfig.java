a679721647888bddbdb47e4ebe93ed346044d5d9
hejz <75412985@qq.com>
2022-03-31 10:21:03 +0800

    添加feign日志配置超时时间

--- order-feign/src/main/java/com/hejz/order/config/FeignConfig.java
+++ order-feign/src/main/java/com/hejz/order/config/FeignConfig.java
@@ -1,16 +1,23 @@
 package com.hejz.order.config;
 
 import feign.Logger;
+import feign.Request;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * feign日志配置
+ * feign配置
  * 全局配置加@Configuration
  * 局部配置不加@Configuration
  */
-//@Configuration
+@Configuration
 public class FeignConfig {
+    //全局超时时间配置
+    @Bean
+    public Request.Options options(){
+        return new Request.Options(5000,10000);
+    }
+    //全局日志配置
     @Bean
     public Logger.Level feignLevelConfig(){
         //所有日志
