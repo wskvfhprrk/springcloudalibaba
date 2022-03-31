9bb407853c74eb00b38e14f899acacebf738c96f
hejz <75412985@qq.com>
2022-03-31 17:52:20 +0800

    配置配置中心如果使用@Value获取配置的，无法感知参数变化，如果需要感知配置文件变需要加注解@RefreshScope

--- ncaos-config/src/main/java/com/hejz/config/controller/ConfigController.java
+++ ncaos-config/src/main/java/com/hejz/config/controller/ConfigController.java
@@ -1,10 +1,12 @@
 package com.hejz.config.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope  //如果需要感知配置文件变需要加注解——这个与spring cloud config一样
 public class ConfigController {
 
     @Value("${user.name}")
